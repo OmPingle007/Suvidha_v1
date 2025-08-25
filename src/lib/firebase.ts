5106ef67ca8414be6122475c605323c4e98ac92b
Om Pingale <ompingle005@gmail.com>
2025-08-25 14:33:01 +0000

    change in firebase functionality

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "suvidha-ocr-landing-page",
-  "appId": "1:625631178267:web:e71052962b11989f11d2be",
-  "storageBucket": "suvidha-ocr-landing-page.firebasestorage.app",
-  "apiKey": "AIzaSyCXW3AiiqlP-pnk5FdKPsgxoiFksCj5VRM",
-  "authDomain": "suvidha-ocr-landing-page.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "625631178267"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
