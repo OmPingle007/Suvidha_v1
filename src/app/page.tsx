96180acedf30d969e43d337952373091bdde3d43
Om Pingale <ompingle005@gmail.com>
2025-08-25 17:04:07 +0000

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import SolutionSection from '@/components/landing/solution-section';
 import UseCasesSection from '@/components/landing/use-cases-section';
 import CtaSection from '@/components/landing/cta-section';
 import Footer from '@/components/landing/footer';
-
+import { Analytics } from "@vercel/analytics/next"
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
